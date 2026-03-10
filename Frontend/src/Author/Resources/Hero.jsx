cbe70d68a548ce14c74125470690fedde5bc82f8
Rima Sah <srima45678@gmail.com>
2026-03-10 11:38:49 +0530

    Added Hero Section in Author / Resources Pannel.

--- Frontend/src/Author/Resources/Hero.jsx
+++ Frontend/src/Author/Resources/Hero.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import './Hero.css';
 
 function Hero() {
-    return ( 
-        <>
-            <h1>Hero</h1>
-        </>
-     );
+    return (
+        <section className="resources-hero">
+            <div className="resources-hero-content">
+                <h1>Exclusive Resources for Authors</h1>
+                <p>
+                    Access whitepapers, guides, style templates, and advanced
+                    research materials curated by our expert community to help
+                    you master the art of storytelling.
+                </p>
+            </div>
+        </section>
+    );
 }
 
 export default Hero;
